f0d85b869693dc7075091e44f7dc852d084a719d
yogeshpm96 <yogeshmahajan500@gmail.com>
2022-12-07 21:17:18 +0530

    code_is_created

---
+++ prime_number.js
@@ -0,0 +1,28 @@
+let num=5;
+
+let count=0;
+   
+for(let i=num; i<=num; i--)
+   
+ 	{
+        
+        if(num%i==0)
+        	
+		{
+            
+            count++;
+		}
+  
+	}
+    
+    
+if(count==2)
+
+        {
+            console.log(Yes Prime Number)
+        }
+
+        else
+        {
+            console.log(Not Prime Number)
+        }
